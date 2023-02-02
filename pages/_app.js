6760d1591c20c73bdce67887f486d8ef865e9567
sivabala142 <sivabala142@gmail.com>
2023-02-02 18:45:21 +0530

    add material ui in dashboard

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
+// import "../styles.css";
 import Head from "next/head";
 import "../styles/default.css";
-import "../styles/module.css";
+// import "../app/global.css"
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -12,12 +14,13 @@ export default function MyApp({ Component, pageProps }) {
   integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
   crossorigin="anonymous"
 />
-<link
-  rel="stylesheet"
-  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-/>
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+
     </Head>
-      <Component {...pageProps} />
+      <Component {...pageProps} />;
     </>
   );
 }
+
--- pages/admin/main.js
+++ pages/admin/main.js
@@ -1,13 +1,7 @@
 import Sidenav from 'Asset/layout/sidenav';
-import Image from 'next/image';
+
 import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import mypic from '../../Asset/Images/colan-logo.png'
+
 const Main = () => {
   return (
  
